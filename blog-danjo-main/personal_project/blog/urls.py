623e14762a5cb949bcc71958594ecbb2bb67e2f7
Dishats <dishats16@gmail.com>
2024-11-20 11:45:52 +0530

    final

--- blog-danjo-main/personal_project/blog/urls.py
+++ blog-danjo-main/personal_project/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),  # Shows all blogs
     path('<int:blog_id>', views.details, name='details'),  # Shows a specific blog by ID
+    path('all_blogs/', views.all_blogs, name='all_blogs'),
 ]
--- blog-danjo-main/personal_project/blog/views.py
+++ blog-danjo-main/personal_project/blog/views.py
@@ -9,3 +9,5 @@ def all_blogs(request):
 def details(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'blog/details.html', {'blog': blog})
+
+
